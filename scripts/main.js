92e23f669d2af4bdb4baebdf78c1b756fcf87144
kevinelong <kevinelong@gmail.com>
2024-05-03 07:56:27 -0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    commonNavigation.innerHTML = `
+    LOGO
+    <a href="./index.html">HOME</a>
+    <a href="./activities.html">ACTIVITIES</a>
+    <a href="./car.html">CAR</a>
+    <a href="./dining.html">DINING</a>
+    <a href="./overnight.html">OVERNIGHT</a>
+    <a href="./about.html">ABOUT</a>   
+    `;
+
+
+});//END LOADED
